4ca72d12529d998940fe53dd824f680d107795cf
MeatValley <81434373+MeatValley@users.noreply.github.com>
2022-10-05 11:28:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+from re import sub
+import myutils as m
+import linecache as lc
+
+t = open('titles.txt')
+they_say_it = open ("they_say_it.txt", "w")
+for i in range (1,39):
+    they_say_it = open ("they_say_it.txt", "a")
+    x = -1
+
+    title = m.generate_the_subtitle_name(i)
+    path = m.generate_the_path(title, i)
+
+    current_title = t.readline()
+    current_title= current_title.lower()
+    current_title= current_title.replace('\n', '')
+
+    time= '\n'
+    line = '\n'+m.get_s0xe0y(i) + ' Title: '+ current_title + '\nthey say it?' 
+
+    f = open(path)
+    flag = False
+    line_counter = 1
+    for i in f:
+        current_line = i.replace('\n', '')
+        current_line = current_line.lower()
+        x = current_line.find(current_title)
+        if(x!=-1):
+            if not flag: line = line + '  YES!'+'\n\n'
+            line+='quote: ' + current_line
+            time = lc.getline(path, line_counter-1)
+            if(time[0] != '0'):
+                time = lc.getline(path, line_counter-2)
+            line += '\nWhen? '+ time+ '\n'
+            flag = True
+        line_counter+=1
+
+
+    if not flag:
+        #maybe is the title is splited in two 
+        part_of_title = ''
+        for i in range(7):
+            part_of_title += current_title[i]
+
+        print(part_of_title)
+        fl, quote2 = m.serach_with_getline(path, part_of_title)
+
+        line +='  Nop :(\n'
+        
+        if fl:
+            line += 'At least they say "' + part_of_title + '"'
+            line+= '\nin: '+ quote2
+    they_say_it.write('\n--------------------------')
+    they_say_it.write(line)
+
---
+++ myutils.py
@@ -0,0 +1,102 @@
+from turtle import position
+
+def get_s0xe0y(i):
+    if (i%10 == 0):
+        s0xe0y = 'S0'+str(int(i/10))+'E10'
+    else:
+        s0xe0y = 'S0'+str(int(i/10 + 1))+'E0'+ str(i%10)
+    return s0xe0y
+
+def generate_the_subtitle_name(i):
+    """generate the title based on the number"""
+    if (i%10 == 0):
+        title = 'Game.Of.Thrones.S0'+str(int(i/10))+'E10.srt'
+    else:
+        title = 'Game.Of.Thrones.S0'+str(int(i/10 + 1))+'E0'+ str(i%10)+'.srt'
+    return title
+
+def generate_the_path(title, i):
+    if (i%10 == 0):
+        path ='subtitles/s'+ str(int(i/10))+'/'+title
+
+    else:
+        path ='subtitles/s'+ str(int(i/10+1))+'/'+title
+
+    return path
+
+
+def find_the_ep_title():
+    """this function receive a file and return the title of that file"""
+    titles = []
+    #s1
+    for i in range(1,10):
+        title = generate_the_subtitle_name(i)
+        path = generate_the_path(title, i)
+        f = open(path)
+        x = f.read()
+        # for c in x:
+        #     if c == '<font color=#00FFFF>':
+        c = ''
+        for i in range (240, 400): 
+            if x[i] == 'F' and x[i+1] == '>':
+                for j in range(100):
+                    # print(x[i+j+2], end='')
+                    c+=x[i+j+2]
+                    if x[i+j+3] == '<':
+                        break
+                titles.append(c)
+                break
+        
+            
+        print('')
+    # print(titles)
+
+    f = open('titles.txt', "w")
+    for title in titles:
+        f.write(title+'\n')
+    f.close
+
+    
+def find_all(str, key):
+    start = 0
+    positions = []
+    count = 0
+    c = 0
+    while(start != -1):
+        x = str.find(key, start)
+        count+=1
+        if x!=-1:
+            positions.append(x)
+            start=x+len(key)
+            c+=1
+        if(start>(len(str)-1) or start == 0 or count==5):
+            start =-1
+
+        # print(start)
+    return positions, c
+
+import linecache as lc
+
+
+def serach_with_getline(file, key):
+    line = 0
+    f = open(file, "r")
+    flag = False
+    quote = ''
+    # print(f)
+    for i in f:
+        k = i.replace('\n', '')
+        k = k.lower()
+        x = k.find(key)
+        if(x!=-1):
+            print(k)
+            quote = k
+            flag = True
+            time = lc.getline(file, line)
+            print(time)
+        line+=1
+    return flag, quote
+
+
+
+
